6df2f4898be7d6dd455d8cd3c4ee66f3e56147d1
Michael Lynch <michael@michaelrlynch.com>
2019-08-06 00:49:04 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase = "Squawk!")
-  print phrase 
-  return phrase
-  puts phrase
+ puts phrase
+ print phrase
+ return phrase
 end
